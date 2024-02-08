34022ad3f7a16020ecd88ba9527ed82780695870
Lin Lin <lin.lin@solarwinds.com>
2024-02-07 16:21:41 -0800

    blank python test for swo

--- sample-apps/blank-python/5-cleanup.sh
+++ sample-apps/blank-python/5-cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eo pipefail
-STACK=blank-python
+STACK=lin-test-blank-python
 if [[ $# -eq 1 ]] ; then
     STACK=$1
     echo "Deleting stack $STACK"
--- sample-apps/blank-python/function/lambda_function.py
+++ sample-apps/blank-python/function/lambda_function.py
@@ -2,12 +2,12 @@ import os
 import logging
 import jsonpickle
 import boto3
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all
+#from aws_xray_sdk.core import xray_recorder
+#from aws_xray_sdk.core import patch_all
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-patch_all()
+#patch_all()
 
 client = boto3.client('lambda')
 client.get_account_settings()
