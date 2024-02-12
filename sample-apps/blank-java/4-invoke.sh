e34dcc8a5e4f14ede58ea3c5ea37524c32fde575
Lin Lin <lin.lin@solarwinds.com>
2024-02-12 15:02:19 -0800

    double the fun for java

--- sample-apps/blank-java/4-invoke.sh
+++ sample-apps/blank-java/4-invoke.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -eo pipefail
 STACK=lin-test-blank-java
-FUNCTION=$(aws cloudformation describe-stack-resource --stack-name $STACK --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
+FUNCTION1=$(aws cloudformation describe-stack-resource --stack-name $STACK --logical-resource-id function1 --query 'StackResourceDetail.PhysicalResourceId' --output text)
+FUNCTION2=$(aws cloudformation describe-stack-resource --stack-name $STACK --logical-resource-id function2 --query 'StackResourceDetail.PhysicalResourceId' --output text)
 
 while true; do
   for name in event-randsleep event-exception; do
-    aws lambda invoke --function-name $FUNCTION --payload "fileb://${name}.json" out.json
+    aws lambda invoke --function-name $FUNCTION1 --payload "fileb://${name}.json" out.json
+    cat out.json
+    echo ""
+  done
+  for name in event; do
+    aws lambda invoke --function-name $FUNCTION2 --payload "fileb://${name}.json" out.json
     cat out.json
     echo ""
   done
