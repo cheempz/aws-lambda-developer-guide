2eea48c0665b54a6cdb44564ea500d65b6f1a13f
Lin Lin <lin.lin@solarwinds.com>
2024-02-13 17:28:41 -0800

    updated layers; added exception and sleep for python

--- sample-apps/blank-python/function/lambda_function.py
+++ sample-apps/blank-python/function/lambda_function.py
@@ -2,6 +2,8 @@ import os
 import logging
 import jsonpickle
 import boto3
+import random
+import time
 #from aws_xray_sdk.core import xray_recorder
 #from aws_xray_sdk.core import patch_all
 
@@ -16,5 +18,18 @@ def lambda_handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES\r' + jsonpickle.encode(dict(**os.environ)))
     logger.info('## EVENT\r' + jsonpickle.encode(event))
     logger.info('## CONTEXT\r' + jsonpickle.encode(context))
+
+    if 'sleepMs' in event.keys():
+        try:
+            sleep_s = int(event.get("sleepMs")) / 1000
+        except ValueError:
+            sleep_s = random.uniform(0, 3.0)
+        logger.info("sleeping %f seconds", sleep_s)
+        time.sleep(sleep_s)
+
+    if 'exception' in event.keys():
+        # throw a division by zero
+        notused = 1/0
+
     response = client.get_account_settings()
     return response['AccountUsage']
